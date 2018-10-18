93d20ee3eaeaa177ea2509222a1f5e310f9f4336
anreh <anurehka@gmail.com>
2018-10-18 12:49:20 +0800

    Added a new comment in mainactivity.java

--- app/src/main/java/com/example/taruc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/taruc/myapplication/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //blahhh blekkk blekkk
+        
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
